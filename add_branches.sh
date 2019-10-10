102f95f1e70bf6ff91fe63f77c3aa68092e0d7f8
kaiba <kaibadash@gmail.com>
2019-10-10 22:46:29 +0900

    Tidy up

--- add_branches.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./versions.sh | curl https://github.com/kaibadash/elasticsearch-kuromoji-dockerfile/tree/123 -o /dev/null -w '%{http_code}\n' -s
--- versions.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-curl -s https://www.docker.elastic.co/\# | grep "docker pull docker.elastic.co/elasticsearch/elasticsearch:" | perl -pe 'if(($_)=/([0-9]+([.][0-9]+)+)/){$_.="\n"}'
-
